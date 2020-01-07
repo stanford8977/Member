b0598add8120972d0cbc2f79b6de97e53f3a26f9
name <miia@com.tw>
2020-01-07 14:36:34 +0800

    1st

--- app/src/main/java/com/fju/member/MainActivity.java
+++ app/src/main/java/com/fju/member/MainActivity.java
@@ -1,14 +1,38 @@
 package com.fju.member;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+
+    private EditText nm;
+    private EditText ag;
+    private EditText gd;
+    private String ageString;
+    private String nameString;
+    private String genderString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nm = findViewById(R.id.nickname1);
+        ag = findViewById(R.id.age1);
+        gd = findViewById(R.id.gender1);
+        nameString = nm.getText().toString();
+        ageString= ag.getText().toString();
+        genderString = gd.getText().toString();
+
+
+
+        if(TextUtils.isEmpty(nameString) && TextUtils.isEmpty(ageString) && TextUtils.isEmpty(genderString)){
+            Intent intent = new Intent(this,NicknameActivity.class);
+            startActivityForResult(intent,0);
+        }
     }
 }
